156d0e7ca95dc1efaf09d8a86a81cbd5cdaf3969
cesar <cursos.mapatupisaro@gamail.com>
2024-02-13 18:19:44 -0600

    Initial Commit

---
+++ DemoNavSwift/DemoNavSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoNavSwiftApp.swift
+//  DemoNavSwift
+//
+//  Created by Aguid Ramirez Sanchez on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoNavSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
